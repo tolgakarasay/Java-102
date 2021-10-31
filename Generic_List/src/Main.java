1a1f5e93dd07f785e90e0ce32782148e4bfa0d7f
Tolga Karasay <tolgakarasay@gmail.com>
2021-10-31 03:18:12 +0300

    Generic List - homework part 2 done

--- Generic_List/src/Main.java
+++ Generic_List/src/Main.java
@@ -1,22 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         MyList<Integer> liste = new MyList<>();
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
         liste.add(10);
         liste.add(20);
         liste.add(30);
+        System.out.println("2. indisteki değer : " + liste.get(2));
+        liste.remove(2);
         liste.add(40);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
-        liste.add(50);
-        liste.add(60);
-        liste.add(70);
-        liste.add(80);
-        liste.add(90);
-        liste.add(100);
-        liste.add(110);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-        System.out.println("Dizinin Kapasitesi : " + liste.getCapacity());
+        liste.set(0, 100);
+        System.out.println("2. indisteki değer : " + liste.get(2));
+        System.out.println(liste.toString());
     }
 }
--- Generic_List/src/MyList.java
+++ Generic_List/src/MyList.java
@@ -1,4 +1,6 @@
-public class MyList<T>{
+import java.util.Arrays;
+
+public class MyList<T> {
     private T[] arr;
     private int capacity;
     private int size = 0;
@@ -8,35 +10,72 @@ public class MyList<T>{
         this.arr = (T[]) new Object[capacity];
     }
 
-    public MyList(int capacity){
+    public MyList(int capacity) {
         this.capacity = capacity;
         this.arr = (T[]) new Object[capacity];
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public int getCapacity(){
+    public int getCapacity() {
         return this.capacity;
     }
 
-    public void add(T data){
-        if(size==capacity){
+    public void add(T data) {
+        if (size == capacity) {
             doubleCapacity();
         }
-        arr[size]=data;
+        arr[size] = data;
         size++;
     }
 
-    private void doubleCapacity(){
+    private void doubleCapacity() {
         this.capacity *= 2;
         T[] temp = (T[]) new Object[capacity];
 
         for (int i = 0; i < size; i++) {
             temp[i] = this.arr[i];
         }
-
         this.arr = temp;
+    }
+
+    public T get(int index) {
+        if (index >= 0 && index < size) {
+            return this.arr[index];
+        }
+        return null;
+    }
+
+    public void remove(int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size - 1; i++) {
+                this.arr[i] = this.arr[i + 1];
+            }
+            this.arr[size - 1] = null;
+            size--;
+        }
+    }
+
+    public void set(int index, T data) {
+        if (index >= 0 && index < size) {
+            this.arr[index] = data;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+
+        for (int i = 0; i < size; i++) {
+            str += this.arr[i];
+            if (i == size - 1) {
+                str += "]";
+            } else {
+                str += ",";
+            }
+        }
+        return str;
     }
 }
